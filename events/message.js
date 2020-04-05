9421c0b178af4f169af1650a255809d71d94a39d
Marcus <marcusnesk@gmail.com>
2020-04-05 21:53:01 +0200

    removed joke from index, fixed message.js

--- events/message.js
+++ events/message.js
@@ -1,20 +1,15 @@
 const joke = require("../commands/joke")
+const kick = require("../commands/kick")
+const taunt = require("../commands/taunt")
+
 module.exports = (client, message) => {
   if (message.content.startsWith("/joke")) {
     return joke(message)
   }
-}
-
-const kick = require("../commands/kick")
-module.exports = (client, message) => {
-  if (message.content.startsWith("!kick")) {
+  else if (message.content.startsWith("!kick")) {
     return kick(message)
   }
-}
-
-const taunt = require("../commands/taunt")
-module.exports = (client, message) => {
-  if (message.content.startsWith("/taunt")) {
+  else if (message.content.startsWith("/taunt")) {
     return taunt(message)
   }
 }
